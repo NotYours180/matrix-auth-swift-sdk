e9496a443212ce7bccfc6003766e57a554112698
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-07-17 10:06:23 -0400

    Create Travis config and script

---
+++ ci/travis.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+xcodebuild -version
+
+xcodebuild \
+    -project $FRAMEWORK_NAME.xcodeproj \
+    -scheme "$FRAMEWORK_NAME macOS" \
+    ONLY_ACTIVE_ARCH=YES \
+    test | xcpretty
+
+xcodebuild \
+    -project $FRAMEWORK_NAME.xcodeproj \
+    -scheme "$FRAMEWORK_NAME iOS" \
+    -sdk iphonesimulator \
+    -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" \
+    ONLY_ACTIVE_ARCH=NO \
+    test | xcpretty
+
+xcodebuild \
+    -project $FRAMEWORK_NAME.xcodeproj \
+    -scheme "$FRAMEWORK_NAME tvOS" \
+    -sdk appletvsimulator \
+    -destination "platform=tvOS Simulator,name=Apple TV 1080p" \
+    ONLY_ACTIVE_ARCH=NO \
+    test | xcpretty
+
+pod lib lint --quick
