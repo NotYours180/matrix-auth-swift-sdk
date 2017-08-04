5e4870c987f86dd8366b99401616399048a06e07
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-04 15:43:31 -0400

    [Travis] Loop through keys as a variable

--- bin/write.sh
+++ bin/write.sh
@@ -2,16 +2,21 @@
 
 BUILD_INFOPLIST_FILE="$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"
 
-if [ -z "$DEV_CLIENT_ID" ] \
-|| [ -z "$DEV_CLIENT_SECRET" ] \
-|| [ -z "$PROD_CLIENT_ID" ] \
-|| [ -z "$PROD_CLIENT_SECRET" ]; then
-    source "$SRCROOT/bin/secrets.sh"
-fi
-
 pb=/usr/libexec/PlistBuddy
 
-for key in DEV_CLIENT_ID DEV_CLIENT_SECRET PROD_CLIENT_ID PROD_CLIENT_SECRET; do
+keys="DEV_CLIENT_ID
+      DEV_CLIENT_SECRET
+      PROD_CLIENT_ID
+      PROD_CLIENT_SECRET"
+
+for key in $keys; do
+    if [ -z "${!key}" ]; then
+        source "$SRCROOT/bin/secrets.sh"
+        break
+    fi
+done
+
+for key in $keys; do
     $pb -c "Delete :$key" "$BUILD_INFOPLIST_FILE"
     $pb -c "Add :$key string ${!key}" "$BUILD_INFOPLIST_FILE"
 done
